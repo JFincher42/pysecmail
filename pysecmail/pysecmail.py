e5f3716448d9d01fc62d0b832bea13196441cc58
Jon Fincher <jfincher42@gmail.com>
2020-10-16 16:46:10 -0500

    Basic UI working, started filling in the blanks

--- pysecmail/pysecmail.py
+++ pysecmail/pysecmail.py
@@ -3,11 +3,20 @@ Generate a secure temporary email address
 Allows you to create, monitor, and delete the address at will
 """
 
-import requests
-import json
+# Imports
+import requests  # To make Web API calls
+import json  # To store and retrieve data
+import sys  # Access to argv
+import pathlib  # Path referencing the right way
+
+from PyQt5.QtWidgets import *  # To manipulate the window
+from PyQt5.QtCore import *  # For core QT functionality
+from PyQt5.QtGui import *  # Gui elements
+
+from MainWindow import Ui_MainWindow  # My main window
 
 
-# API info
+# API info - may be expanded later
 endpoint = "https://www.1secmail.com/api/v1/"
 domain = "1secmail.com"
 
@@ -20,11 +29,43 @@ email_domains = [
     "esiix.com",
 ]
 
+# Configuration info
+
+# QT Main Window
+class MainWindow(QMainWindow, Ui_MainWindow):
+    """Main Window for the application
+
+    Args:
+        QMainWindow ([type]): QT Main Window
+        UI_MainWindow ([type]): My designed main window
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # Setup some basics here
+        self.cboDomain.addItems(email_domains)
+
+        # Get the list of existing email addresses
+        self.email_addresses = self.get_email_addresses()
+
+    
+    def get_email_addresses(self):
+        
 
 # Functions
-def generate_email_address(count: int) -> list:
+def generate_email_address(count: int = 1) -> list:
+    """Generate one or more email
+    addresses to use
+
+    Args:
+        count (int): How many email addresses to create (default 1)
+
+    Returns:
+        list: A list of email addresses
     """
-    """
+
     pass
 
 
@@ -51,3 +92,15 @@ def get_messages(email: str) -> list:
         for field, value in email_message.items():
             print(f"{field}: {value}")
         print()
+
+
+# Main entry point
+if __name__ == "__main__":
+
+    # The one and only QT App
+    app = QApplication(sys.argv)
+
+    # The main window
+    window = MainWindow()
+    window.show()
+    app.exec()
